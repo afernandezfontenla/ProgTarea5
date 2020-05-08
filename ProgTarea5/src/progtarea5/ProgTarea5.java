c01e739e8129ce7af56fb8af02b8990487521a97
afernandezfontenla <56158212+afernandezfontenla@users.noreply.github.com>
2020-05-08 14:44:11 +0200

    Add files via upload

---
+++ ProgTarea5/src/progtarea5/ProgTarea5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progtarea5;
+
+/**
+ *
+ * @author Nuevo usuario
+ */
+public class ProgTarea5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora cal = new Calculadora();
+        cal.setVisible(true);
+        cal.mostrarVentana();
+        //cal.nuevaPosicion();
+    }
+    
+}
